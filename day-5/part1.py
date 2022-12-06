7def19bce87586fcf1a06dd6d665b267425d099c
Dean Daskalantonakis <dean.daskalantonakis@activeprospect.com>
2022-12-06 11:59:14 -0600

    completed day 5

---
+++ day-5/part1.py
@@ -0,0 +1,58 @@
+# https://adventofcode.com/2022/day/5
+with open('/Users/ddaskal/git/advent-of-code-2022/day-5/input.txt') as input_file:
+    init = True
+    init_stacks = []
+    build_stacks = []
+    stacks = [[], [], [], [], [], [], [], [], []]
+    tops = ""
+
+    for line in input_file:
+        if init:
+            if line != '\n':
+                # init stacks
+                line = line.strip('\n')
+                line += ' ' * (35 - len(line))
+                init_stacks.append(line)
+            elif line == '\n':
+                # set up stacks proper
+                init_stacks.pop()  # removes line with naming stacks
+                build_stacks = init_stacks[:: -1]
+
+                for entry in build_stacks:
+                    if entry[1] != " ":
+                        stacks[0].append(entry[1])
+                    if entry[5] != " ":
+                        stacks[1].append(entry[5])
+                    if entry[9] != " ":
+                        stacks[2].append(entry[9])
+                    if entry[13] != " ":
+                        stacks[3].append(entry[13])
+                    if entry[17] != " ":
+                        stacks[4].append(entry[17])
+                    if entry[21] != " ":
+                        stacks[5].append(entry[21])
+                    if entry[25] != " ":
+                        stacks[6].append(entry[25])
+                    if entry[29] != " ":
+                        stacks[7].append(entry[29])
+                    if entry[33] != " ":
+                        stacks[8].append(entry[33])
+
+                init = False
+                continue
+
+        # perform moving logic
+        if not init:
+            command = line.strip('\n').split(" ")
+            count = int(command[1])
+            source = int(command[3])-1
+            target = int(command[5])-1
+
+            for i in range(count):
+                stacks[target].append(stacks[source].pop())
+
+    for stack in stacks:
+        tops += stack.pop()
+
+    print(tops)
+    # answer WCZTHTMPS
